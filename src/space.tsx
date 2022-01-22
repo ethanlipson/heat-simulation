02379d92b21473dbfb2addb8f35ad7832c57a3fb
Ethan Lipson <ethan.lipson@gmail.com>
2022-01-22 09:39:51 -0500

    Works with high precision float

--- src/space.tsx
+++ src/space.tsx
@@ -45,8 +45,7 @@ const screenTextureFragmentSource = `#version 300 es
   out vec4 FragColor;
 
   void main() {
-    FragColor = texture(uTexture, uv);
-    // FragColor = vec4(texelFetch(uTexture, ivec2(0, 0), 0).r, 0, 0, 1);
+    FragColor = vec4(texture(uTexture, uv).rrr, 1);
   }
 `;
 
@@ -81,12 +80,31 @@ const temperatureFragmentSource = `#version 300 es
   precision mediump float;
 
   uniform sampler2D uTexture;
+  uniform int screenWidth;
+  uniform int screenHeight;
   
   out vec4 FragColor;
 
   void main() {
-    FragColor = vec4(texelFetch(uTexture, ivec2(gl_FragCoord), 0).r + 0.01, 0, 0, 1);
-    // FragColor = vec4(1, 0, 0, 1);
+    int x = int(gl_FragCoord.x);
+    int y = int(gl_FragCoord.y);
+    float currentValue = texelFetch(uTexture, ivec2(x, y), 0).r;
+    float sum = 0.0;
+
+    if (x > 0) {
+      sum += texelFetch(uTexture, ivec2(x - 1, y), 0).r - currentValue;
+    }
+    if (x < screenWidth - 1) {
+      sum += texelFetch(uTexture, ivec2(x + 1, y), 0).r - currentValue;
+    }
+    if (y > 0) {
+      sum += texelFetch(uTexture, ivec2(x, y - 1), 0).r - currentValue;
+    }
+    if (y < screenHeight - 1) {
+      sum += texelFetch(uTexture, ivec2(x, y + 1), 0).r - currentValue;
+    }
+
+    FragColor = vec4(currentValue + 0.2 * sum, 0, 0, 1);
   }
 `;
 
@@ -98,7 +116,6 @@ export default class Space {
   screenTextureShader: Shader;
   currentScreenTexture: WebGLTexture;
   nextScreenTexture: WebGLTexture;
-  currentScreenTextureFBO: WebGLFramebuffer;
   nextScreenTextureFBO: WebGLFramebuffer;
 
   temperatureVAO: WebGLVertexArrayObject;
@@ -113,6 +130,8 @@ export default class Space {
     this.screenHeight = screenHeight;
     gl.viewport(0, 0, screenWidth, screenHeight);
 
+    gl.getExtension('EXT_color_buffer_float');
+
     /********************* Current screen texture **********************/
     this.screenTextureVAO = gl.createVertexArray()!;
 
@@ -122,35 +141,38 @@ export default class Space {
       screenTextureFragmentSource
     );
 
+    const data = new Float32Array(screenWidth * screenHeight);
+    for (let x = 0; x < screenWidth; x++) {
+      for (let y = 0; y < screenHeight; y++) {
+        if (
+          Math.pow(x - screenWidth / 2, 2) +
+            Math.pow(y - screenHeight / 2, 2) <=
+          50 * 50
+        ) {
+          data[x + y * screenWidth] = 1;
+        }
+      }
+    }
+
     this.currentScreenTexture = gl.createTexture()!;
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, this.currentScreenTexture);
     gl.texImage2D(
       gl.TEXTURE_2D,
       0,
-      gl.RGBA8,
+      gl.R32F,
       screenWidth,
       screenHeight,
       0,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      null
+      gl.RED,
+      gl.FLOAT,
+      data
     );
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-    this.currentScreenTextureFBO = gl.createFramebuffer()!;
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.currentScreenTextureFBO);
-    gl.framebufferTexture2D(
-      gl.FRAMEBUFFER,
-      gl.COLOR_ATTACHMENT0,
-      gl.TEXTURE_2D,
-      this.currentScreenTexture,
-      0
-    );
 
     /********************* Next screen texture **********************/
     this.nextScreenTexture = gl.createTexture()!;
@@ -159,12 +181,12 @@ export default class Space {
     gl.texImage2D(
       gl.TEXTURE_2D,
       0,
-      gl.RGBA8,
+      gl.R32F,
       screenWidth,
       screenHeight,
       0,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
+      gl.RED,
+      gl.FLOAT,
       null
     );
 
@@ -202,34 +224,46 @@ export default class Space {
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.nextScreenTextureFBO);
     gl.bindVertexArray(this.temperatureVAO);
     this.temperatureShader.use(gl);
+    gl.uniform1i(
+      gl.getUniformLocation(this.temperatureShader.program, 'uTexture'),
+      0
+    );
+    gl.uniform1i(
+      gl.getUniformLocation(this.temperatureShader.program, 'screenWidth'),
+      this.screenWidth
+    );
+    gl.uniform1i(
+      gl.getUniformLocation(this.temperatureShader.program, 'screenHeight'),
+      this.screenHeight
+    );
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     /********************* Swap current and next textures **********************/
-    // gl.copyTexImage2D(
-    //   gl.TEXTURE_2D,
-    //   0,
-    //   gl.RGBA,
+    gl.copyTexImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.R32F,
+      0,
+      0,
+      this.screenWidth,
+      this.screenHeight,
+      0
+    );
+
+    // gl.bindFramebuffer(gl.READ_FRAMEBUFFER, this.nextScreenTextureFBO);
+    // gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.currentScreenTextureFBO);
+    // gl.blitFramebuffer(
     //   0,
     //   0,
     //   this.screenWidth,
     //   this.screenHeight,
-    //   0
+    //   0,
+    //   0,
+    //   this.screenWidth,
+    //   this.screenHeight,
+    //   gl.COLOR_BUFFER_BIT,
+    //   gl.NEAREST
     // );
-
-    gl.bindFramebuffer(gl.READ_FRAMEBUFFER, this.nextScreenTextureFBO);
-    gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.currentScreenTextureFBO);
-    gl.blitFramebuffer(
-      0,
-      0,
-      this.screenWidth,
-      this.screenHeight,
-      0,
-      0,
-      this.screenWidth,
-      this.screenHeight,
-      gl.COLOR_BUFFER_BIT,
-      gl.NEAREST
-    );
 
     /********************* Draw current texture to screen **********************/
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
